fba8b3adb142bac218004f0cfb41b436202ef6f7
volesh <volesh2@gmail.com>
2023-02-08 14:50:31 +0200

    finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 import { AppController } from './app.controller';
@@ -10,13 +11,16 @@ import { ArticlesModule } from './articles/articles.module';
 @Module({
   imports: [
     ArticlesModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: +process.env.POSTGRES_PORT || 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_NAME || 'postgres',
       models: [Articles],
       autoLoadModels: true,
     }),
